5a8fc3ecf88dfda03ad0102a1a195e9718ed23e3
tlarbals824 <simkm0824@naver.com>
2023-03-25 13:03:52 +0900

    feat : add config

---
+++ src/main/java/com/kusithms/kukathon/KukathonApplication.java
@@ -0,0 +1,13 @@
+package com.kusithms.kukathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusithms/kukathon/KukathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusithms.kukathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
